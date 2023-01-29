f591b3c6e925c7afb2d216456dcaf52205743c85
Vladimir Morar <morvladimir1981@gmail.com>
2023-01-29 17:20:14 +0300

    add example 001

---
+++ example001/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+System.Console.WriteLine("Введите целое пятизначное число: ");
+string number = Console.ReadLine();
+
+if(number.Length == 5)
+{
+    if(number[0] == number[4] && number[1] == number[3])
+    {
+        System.Console.WriteLine("да");
+    }
+    else
+    {
+        System.Console.WriteLine("нет");
+    }
+}
+else
+{
+    System.Console.WriteLine("Введено не корректное число!");
+}
